2ebe88b7c6957ce10acebeee94e8e84ef86320c1
bulatovad <damira.bulatova@gmail.com>
2023-05-12 22:46:20 +0600

    solution

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,15 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if(str==null) return false;
+        if(str.startsWith("0") && !str.matches("0[.][0-8]+")) return false;
+
+        try {
+            double number = Double.parseDouble(str);
+            return  number>0;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
     }
 }
